17c3c11606aa1e69f99fcf1c1f3186d4fb6eb6d0
Usman Mazhar <139316450+UsmanChaudhary115@users.noreply.github.com>
2025-01-27 11:47:18 +0500

    Delete templatedStack directory

--- templatedStack/main.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-using namespace std;
-#include"templatedStack.h"
-int main()
-{
-	return 0;
-}
--- templatedStack/templatedStackImplementation.cpp
+++
@@ -1,4 +0,0 @@
-#include"templatedStack.cpp"
-template class Stack<int>;
-template class Stack<char>;
-template class Stack<float>;
